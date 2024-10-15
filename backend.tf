9aca26c39e091b8ee65e5dd211cee2cacc678978
egrage <tygrins@gmail.com>
2024-10-14 22:33:00 -0400

    commit changes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "infra/terraform.state"
+    key            = "tooling/terraform.state"
     bucket         = "dee-bucket-002"
     region         = "us-west-2"
     dynamodb_table = "dee-tablelock-002"
